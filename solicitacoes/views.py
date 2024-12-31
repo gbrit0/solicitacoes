918def710767a5aa6f78e0087c5e18535ecc7668
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2024-12-31 09:48:20 -0300

    Melhorias no insert para evitar SQL Injection e correções no tratamento de exceções

--- solicitacoes/views.py
+++ solicitacoes/views.py
@@ -33,7 +33,7 @@ def criar_solicitacao(request):
                             AND C1_NUM LIKE '[0-9]%';
 
                         """)
-                        ultimo_num = cursor.fetchall()
+                        ultimo_num = cursor.fetchone()
                         
                 proximo_num = str(int(ultimo_num[0][0]) + 1).zfill(6) if ultimo_num else '000001'
                 solicitacao_form.c1_num = proximo_num
@@ -63,68 +63,80 @@ def criar_solicitacao(request):
                                 
                                 instance.c1_filent = '0101'
                                 
-                                insert = (
+                                # insert = (
+                                #     f"BEGIN TRY "
+                                #         f"BEGIN TRANSACTION; "
+                                #         f"INSERT INTO SC1010 WITH (TABLOCKX)"
+                                #         f"(C1_FILIAL, C1_NUM, C1_ITEM, C1_DESCRI, C1_CC, C1_PRODUTO, "
+                                #         f"C1_LOCAL, C1_QUANT, C1_EMISSAO, C1_UM, C1_FILENT, "
+                                #         f"C1_DATPRF, C1_SOLICIT, C1_XOBMEMO, R_E_C_N_O_, C1_XSOLWEB)"
+                                #         f"VALUES ( "
+                                #             f"'{solicitacao_form.c1_filial}', "
+                                #             f"'{solicitacao_form.c1_num}', "
+                                #             f"'{instance.c1_item}', "
+                                #             f"'{produto}', "
+                                #             f"'{instance.c1_cc}', "
+                                #             f"'{instance.c1_produto}', "
+                                #             f"'{instance.c1_local}', "
+                                #             f"'{instance.c1_quant}', "
+                                #             f"'{str(solicitacao_form.c1_emissao).replace('-', '')[:8]}', "
+                                #             f"'{instance.c1_um}', "
+                                #             f"'{instance.c1_filent}', "
+                                #             f"'{str(instance.c1_datprf).replace('-', '')}', "
+                                #             f"'{solicitacao_form.c1_solicit}', "
+                                #             f"CONVERT(VARBINARY(MAX), '{instance.c1_obs}'), "
+                                #             f"'{instance.r_e_c_n_o}', "
+                                #             f"'{solicitacao_form.user.cpf}'); "
+                                        
+                                #         f"COMMIT; "
+                                #     f"END TRY "
+                                #     f"BEGIN CATCH "
+                                #         f"ROLLBACK;"
+                                #         f"THROW; "
+                                #     f"END CATCH; "
+                                # )
+                                
+                                cursor.execute((
                                     f"BEGIN TRY "
                                         f"BEGIN TRANSACTION; "
                                         f"INSERT INTO SC1010 WITH (TABLOCKX)"
                                         f"(C1_FILIAL, C1_NUM, C1_ITEM, C1_DESCRI, C1_CC, C1_PRODUTO, "
                                         f"C1_LOCAL, C1_QUANT, C1_EMISSAO, C1_UM, C1_FILENT, "
                                         f"C1_DATPRF, C1_SOLICIT, C1_XOBMEMO, R_E_C_N_O_, C1_XSOLWEB)"
-                                        f"VALUES ( "
-                                            f"'{solicitacao_form.c1_filial}', "
-                                            f"'{solicitacao_form.c1_num}', "
-                                            f"'{instance.c1_item}', "
-                                            f"'{produto}', "
-                                            f"'{instance.c1_cc}', "
-                                            f"'{instance.c1_produto}', "
-                                            f"'{instance.c1_local}', "
-                                            f"'{instance.c1_quant}', "
-                                            f"'{str(solicitacao_form.c1_emissao).replace('-', '')[:8]}', "
-                                            f"'{instance.c1_um}', "
-                                            f"'{instance.c1_filent}', "
-                                            f"'{str(instance.c1_datprf).replace('-', '')}', "
-                                            f"'{solicitacao_form.c1_solicit}', "
-                                            f"CONVERT(VARBINARY(MAX), '{instance.c1_obs}'), "
-                                            f"'{instance.r_e_c_n_o}', "
-                                            f"'{solicitacao_form.user.cpf}'); "
-                                        
+                                        f"VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CONVERT(VARBINARY(MAX), ?), ?, ? ); "
                                         f"COMMIT; "
                                     f"END TRY "
                                     f"BEGIN CATCH "
                                         f"ROLLBACK;"
                                         f"THROW; "
                                     f"END CATCH; "
-                                )
-                                
-                                cursor.execute(insert)
+                                ), (solicitacao_form.c1_filial, solicitacao_form.c1_num, instance.c1_item, produto, instance.c1_cc,
+                                    instance.c1_produto, instance.c1_local, instance.c1_quant, str(solicitacao_form.c1_emissao).replace('-', '')[:8],
+                                    instance.c1_um, instance.c1_filent, str(instance.c1_datprf).replace('-', ''), solicitacao_form.c1_solicit,
+                                    instance.c1_obs, instance.r_e_c_n_o, solicitacao_form.user.cpf))
 
+                                instance.save()
                                 # conexao.commit() # no sql já tem o commit, testar se insere normalmente
                             except pyodbc.Error as e:
                                 erros.append({
                                     'produto': produto,
                                     'erro': e
                                 })
-                                continue
-                            else:
-                                instance.save()
 
-                            finally:
-                                if erros:
-                                    for erro in erros:
-                                        messages.error(request, f"Não foi possível cadastrar a solicitação para o produto {erro['produto']}. Tente novamente mais tarde. ERRO: {erro['erro']}")
-                                    return redirect('lista_solicitacoes')
                             
-                messages.success(request, "Solicitação cadastrada com sucesso!")
+                if erros:
+                    for erro in erros:
+                        messages.error(request, f"Não foi possível cadastrar a solicitação para o produto {erro['produto']}. Tente novamente mais tarde. ERRO: {erro['erro']}")
+                else:
+                    messages.success(request, "Solicitação cadastrada com sucesso!")
+                            
                 return redirect('lista_solicitacoes')  
                 
             except Exception as e:
                 messages.error(request, f"Erro ao criar solicitação, por favor contate o admnistrador. ERRO: {e}")
-                # return render(request, 'solicitacoes/criar_solicitacao.html', {
-                #     'solicitacao_form': solicitacao_form,
-                #     'formset': formset,
-                #     'errors': formset.errors  
-                # })
                 return redirect('lista_solicitacoes')
+            
+            
         else:
             return render(request, 'solicitacoes/criar_solicitacao.html', {
                 'solicitacao_form': solicitacao_form,
