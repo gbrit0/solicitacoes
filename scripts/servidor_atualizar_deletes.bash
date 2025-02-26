eb692be1756b264591078a0d1a2070a04116e0c2
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2025-02-26 15:32:30 -0300

    Subir a branch legendas

--- scripts/servidor_atualizar_deletes.bash
+++ scripts/servidor_atualizar_deletes.bash
@@ -5,7 +5,7 @@ do
    cd /home/bot/solicitacoes
    printf "\n$(date) Deleção de Solicitações de Compras Inicializada\n"
    source venv/bin/activate
-   python3 solicitacoes/scheduler.py >> deleteSC.log
+   python3 utils/buscaSCDeletadas.py >> /home/bot/solicitacoes/logs/deleteSC.log
    printf "$(date) Deleção de Solicitações de Compras Finalizada\n"
    sleep 900
 done
